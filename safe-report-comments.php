b8a6312ebd4a8aae051db36ea73c975e20e3b335
Daniel Bachhuber <d@danielbachhuber.com>
2012-11-22 01:46:10 +0000

    Use home_url() for mapped domains, but admin_url() where the domain isn't mapped.
    Also allow ajaxurl to be filtered for other situations
    
    Closes #1

--- safe-report-comments.php
+++ safe-report-comments.php
@@ -112,8 +112,16 @@ if ( !class_exists( "Safe_Report_Comments" ) ) {
 
 		public function action_enqueue_scripts() {
 
+			// Use home_url() if domain mapped to avoid cross-domain issues
+			if ( home_url() != site_url() )
+				$ajaxurl = home_url( '/wp-admin/admin-ajax.php' );
+			else
+				$ajaxurl = admin_url( 'admin-ajax.php' );
+
+			$ajaxurl = apply_filters( 'safe_report_comments_ajax_url', $ajaxurl );
+
 			wp_enqueue_script( $this->_plugin_prefix . '-ajax-request', $this->plugin_url . '/js/ajax.js', array( 'jquery' ) );
-			wp_localize_script( $this->_plugin_prefix . '-ajax-request', 'SafeCommentsAjax', array( 'ajaxurl' => home_url( '/wp-admin/admin-ajax.php' ) ) ); // slightly dirty but needed due to possible problems with mapped domains
+			wp_localize_script( $this->_plugin_prefix . '-ajax-request', 'SafeCommentsAjax', array( 'ajaxurl' => $ajaxurl ) ); // slightly dirty but needed due to possible problems with mapped domains
 		}
 
 		public function add_test_cookie() {
